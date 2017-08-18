5be0a9361427a12ab8f6b69475e10e863ca97516
Haotian Chen <haotianchen@HaotiandeMacBook-Air.local>
2017-08-17 20:46:34 -0700

    Initial Commit

---
+++ SocialProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocialProject
+//
+//  Created by Haotian Chen on 2017/8/17.
+//  Copyright © 2017年 Haotian Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocialProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SocialProject
+//
+//  Created by Haotian Chen on 2017/8/17.
+//  Copyright © 2017年 Haotian Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
